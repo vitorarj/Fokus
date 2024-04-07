b74bd91fc9a0e408574a6ba94b7edcf58d3253e9
Vitor Almeida <vitorarj@ufrn.edu.br>
2024-04-07 16:13:54 -0300

    feat: creating variables

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const startBt = document.querySelector('.app__card-primary-button')
+
+//
+const banner = document.querySelector('.app__image')
+const displayTime = document.querySelector('.app__card-timer')
+const titulo = document.querySelector('.app__title')
+
+
+//times value
+const focoInitValue = 1500
+const descansoCurtoInitValue = 300
+const descansoLongoInitValue = 900
+
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
